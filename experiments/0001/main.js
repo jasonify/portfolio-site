bc497bc26318fa16a1d34a321a2ad083570805f7
jason <bjason@gmail.com>
2016-08-16 02:06:11 -0700

    translate

--- experiments/0001/main.js
+++ experiments/0001/main.js
@@ -7,4 +7,15 @@ window.onload = function(){
 
 
 
+      context.fillStyle="#8BF28B";
+      var scale = width/3;
+      console.log('scale', scale);
+
+      for(var angle  = 0; angle < Math.PI * 2; angle+=0.01){
+
+        var x = angle * scale;
+            y = Math.sin(angle) * scale;
+          context.fillRect(x,y, 3,3);
+      }
+
 };
