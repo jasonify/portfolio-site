0f5c7219e09099037e545d3e39d01f58c1cd340d
jason <bjason@gmail.com>
2016-08-16 02:17:50 -0700

    Draw sine wave prototype

--- experiments/0001/main.js
+++ experiments/0001/main.js
@@ -5,26 +5,37 @@ window.onload = function(){
       width = canvas.width = window.innerWidth,
       height = canvas.height = window.innerHeight
 
-      context.translate(0, height/2);
-
-      context.fillStyle="#8BF28B";
-
-
-
-      var scale = height/3;
-      console.log('scale', scale);
-
-      for(var angle  = 0; angle < Math.PI * 2; angle+=0.01){
-
-        var x = angle * scale;
-            y = Math.sin(angle) * scale;
-          context.fillRect(x,y, 3,3);
-      }
-      setTimeout(function(){
+      var clear = function(){
         console.log('width', width);
         console.log('height', height);
-        context.translate(0, -height/2);
+        //context.translate(0, -height/2);
         context.clearRect(0, 0, width, height);
-      }, 100);
+      };
+
+
+      var drawLine = function(scale) {
+
+        // context.translate(0, height/2);
+        context.fillStyle="#8BF28B";
+
+        console.log('scale', scale);
+
+        for(var angle  = 0; angle < Math.PI * 2; angle+=0.01){
+          var x = angle * scale;
+          y = Math.sin(angle) * scale;
+          context.fillRect(x,y, 3,3);
+        }
+      };
+
+      var scale = height/3;
+      drawLine(scale);
+
+      document.body.addEventListener('mousemove', function(e){
+        console.log('e', e);
+        context.translate(0, height/2);
+        drawLine(e.clientX);
+        context.translate(0, -height/2);
+
+      });
 
 };
