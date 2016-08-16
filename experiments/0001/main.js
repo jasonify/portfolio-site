9d33a0c4283b2d41376ea2d8320d5ca41a71ef31
jason <bjason@gmail.com>
2016-08-16 02:12:01 -0700

    clearing but kinda wrong

--- experiments/0001/main.js
+++ experiments/0001/main.js
@@ -5,10 +5,13 @@ window.onload = function(){
       width = canvas.width = window.innerWidth,
       height = canvas.height = window.innerHeight
 
-
+      context.translate(0, height/2);
 
       context.fillStyle="#8BF28B";
-      var scale = width/3;
+
+
+
+      var scale = height/3;
       console.log('scale', scale);
 
       for(var angle  = 0; angle < Math.PI * 2; angle+=0.01){
@@ -17,5 +20,11 @@ window.onload = function(){
             y = Math.sin(angle) * scale;
           context.fillRect(x,y, 3,3);
       }
+      setTimeout(function(){
+        console.log('width', width);
+        //console.log('height', height);
+        context.translate(0, -height/2);
+        context.clearRect(0, 0, width, height);
+      }, 100);
 
 };
