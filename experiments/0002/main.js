9afaec1b637560715abf4f56e241e5e574286c69
jason <bjason@gmail.com>
2016-08-17 23:49:16 -0700

    toggle

--- experiments/0002/main.js
+++ experiments/0002/main.js
@@ -1,10 +1,41 @@
 window.onload = function(){
   console.log('Loaded');
-  var canvas = document.getElementById('canvas'),
-      context = canvas.getContext('2d'),
-      width = canvas.width = window.innerWidth,
-      height = canvas.height = window.innerHeight
+
+
+  var toggled = true;
 
 
 
+  document.addEventListener('mousedown', function(){
+    toggled = !toggled;
+    console.log('toggled');
+
+  });
+
+
+  var counter = 0;
+
+    var render = function(){
+      var bee = document.getElementById('bee');
+      var body = document.body;
+
+    counter++;
+    if(counter >= 100) counter = 0;
+    var bg1 = 'black';
+    var bg2 = 'linear-gradient(to bottom, #fefcea 0%,#f1da36 100%)'
+
+    requestAnimationFrame(render);
+
+    if(toggled){
+      body.style.background = bg1;
+      bee.style.background =bg2;
+    } else {
+      body.style.background = bg2;
+      bee.style.background =bg1;
+
+    }
+  }
+
+
+  render();
 };
