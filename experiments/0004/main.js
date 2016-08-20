752f9f82ec410254e02a133ec35acc629e975df5
jason <bjason@gmail.com>
2016-08-20 00:35:13 -0700

    Translate

--- experiments/0004/main.js
+++ experiments/0004/main.js
@@ -4,17 +4,38 @@ window.onload = function(){
       context = canvas.getContext('2d'),
       width = canvas.width = window.innerWidth,
       height = canvas.height = window.innerHeight,
-      radius = width / 3,
+      radius = width / 4,
+      angle = 0,
+      speed = 0.05,
       objSize = 15;
 
 
+      var render = function() {
 
-      context.translate(width /2  ,  height /2)
-      context.fillStyle='black';
-      context.beginPath();
-      context.arc(0, 0, objSize, 0, 2 * Math.PI);
-      context.stroke();
-      context.fill();
+        context.restore();
+        context.clearRect(0, 0, width, height);
+        context.save();
+        context.translate(width /2  ,  height /2)
+        context.fillStyle='black';
+        context.beginPath();
+
+        var x = radius * Math.sin(angle);
+        var y =  radius * Math.cos(angle);
+        console.log('x', x, 'y', y);
+
+        context.arc(x, y, objSize, 0, 2 * Math.PI);
+        context.stroke();
+        context.fill();
+
+        angle+=speed;
+        console.log('angle', angle);
+
+        requestAnimationFrame(render);
+       
+      }
+
+      render();
+
 
 
 
