fe206e3910e4f19a14fe273b116ee1785f9562b7
jason <bjason@gmail.com>
2016-08-22 23:37:55 -0700

    draw lil strands

--- experiments/0007/main.js
+++ experiments/0007/main.js
@@ -1,14 +1,23 @@
 window.onload = function(){
   console.log('Loaded');
+
   var canvas = document.getElementById('canvas'),
       context = canvas.getContext('2d'),
       width = canvas.width = window.innerWidth,
-      height = canvas.height = window.innerHeight
+      height = canvas.height = window.innerHeight,
+      angle = 0,
+      speed = 0.01
+
 
       var ctx = context;
+      var img = new Image();
+
+        img.src = 'images/jelly.png';
+
 
 
       var drawPoints = function(points) {
+        
 
         ctx.beginPath();
         // move to the first point
@@ -25,6 +34,7 @@ window.onload = function(){
         ctx.quadraticCurveTo(points[i].x, points[i].y, points[i+1].x,points[i+1].y);
 
         ctx.strokeStyle = '#E4AFFF';
+        ctx.lineCap = "round";
         ctx.lineWidth = 6;
         ctx.stroke();
     }
@@ -35,8 +45,30 @@ window.onload = function(){
     points.push({x: 400, y: 150});
     points.push({x: 100, y: 250});
 
-    // ctx.translate(300, 300);
-    drawPoints(points);
+    //ctx.translate(300, 300);
+    //drawPoints(points);
+
+    var render = function() {
+
+      ctx.restore();
+      var points1 = [];
+      for(var i = 0; i < 10 ; i++){
+        var x = i;
+        points1.push({x: x, y: -img.height*0.055  + i*20});
+      }
+
+      ctx.save();
+      ctx.translate(width/2, height/2);
+      drawPoints(points1);
+      ctx.drawImage(img,  -img.width/2 
+      , -img.height /2 , img.width, img.height);
+    }
+
+    //context.drawImage(img, width/2-img.width/2, height/2-img.height/2, img.width, img.height);
+    img.onload = function() {
+      console.log('img loaded');
+      render();
+    }
 
 
 };
