37a7a30bf1b329b2349cbe9bd332ce6088911ecc
jason <bjason@gmail.com>
2016-08-23 23:59:16 -0700

    shaders

--- experiments/0008/main.js
+++ experiments/0008/main.js
@@ -1,10 +1,39 @@
-window.onload = function(){
-  console.log('Loaded');
-  var canvas = document.getElementById('canvas'),
-      context = canvas.getContext('2d'),
-      width = canvas.width = window.innerWidth,
-      height = canvas.height = window.innerHeight
+
+function getShader(gl, id, type) {
+  var shaderScript, theSource, currentChild, shader;
+
+  shaderScript = document.getElementById(id);
+
+  if (!shaderScript) {
+    return null;
+  }
+
+  theSource = shaderScript.text;
+
+  if (!type) {
+    if (shaderScript.type == "x-shader/x-fragment") {
+      type = gl.FRAGMENT_SHADER;
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+      type = gl.VERTEX_SHADER;
+    } else {
+      // Unknown shader type
+      return null;
+    }
+  }
+  shader = gl.createShader(type);
 
 
+  gl.shaderSource(shader, theSource);
 
-};
+  // Compile the shader program
+  gl.compileShader(shader);  
+
+  // See if it compiled successfully
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {  
+    alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));  
+    gl.deleteShader(shader);
+    return null;  
+  }
+
+  return shader;
+}
