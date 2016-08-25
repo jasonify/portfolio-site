676f38c72aa79db00305e204b634dcee211b326c
jason <bjason@gmail.com>
2016-08-25 04:39:07 -0700

    mouse

--- experiments/0009/main.js
+++ experiments/0009/main.js
@@ -1,150 +1,36 @@
-// Modified slightly from webgl tutorial / class.
 window.onload = function(){
   console.log('Loaded');
 
   var canvas = document.getElementById('canvas'),
+      context = canvas.getContext('2d'),
       width = canvas.width = window.innerWidth,
-        height = canvas.height = window.innerHeight;
+      height = canvas.height = window.innerHeight,
+      angle = 0,
+      speed = 0.01
 
 
-        (function() {
-          var gl,
-          shaderProgram,
-          vertices,
-          vertexCount = 1000,
-            mouseX = 0,
-              mouseY = 0;
+      var ctx = context;
+      var img = new Image();
 
-              initGL();
-              createShaders();
-              createVertices();
-              draw();
-
-              canvas.addEventListener("mousemove", function(event) {
-                mouseX = map(event.clientX, 0, canvas.width, -1, 1);
-                mouseY = map(event.clientY, 0, canvas.height, 1, -1);
-              });
+      img.src = 'mouse.png';
 
 
-              function initGL() {
-                var canvas = document.getElementById("canvas");
-                gl = canvas.getContext("webgl");
+      img.addEventListener('load', function(){
 
-                gl.viewport(0, 0, canvas.width, canvas.height);
-                gl.clearColor(0.98, 0.75, 0.8, 1);
-              }
+        var render = function(){
+          ctx.restore();
+          ctx.save();
 
-              function createShaders() {
-                var vertexShader = getShader(gl, "shader-vs");
+          ctx.translate( width / 2, height / 2);
+          ctx.drawImage(img,-img.width/2, -img.height/2, img.width, img.height);
 
-                var fragmentShader = getShader(gl, "shader-fs");
-
-                shaderProgram = gl.createProgram();
-                gl.attachShader(shaderProgram, vertexShader);
-                gl.attachShader(shaderProgram, fragmentShader);
-                gl.linkProgram(shaderProgram);
-                gl.useProgram(shaderProgram);
-              }
-
-              function createVertices() {
-
-                vertices = [];
-                for(var i = 0; i < vertexCount; i++) {
-                  vertices.push(Math.random() * 2 - 1);
-                  vertices.push(Math.random() * 2 - 1);
-                }
-
-                var buffer = gl.createBuffer();
-                gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-                gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-                var coords = gl.getAttribLocation(shaderProgram, "coords");
-                //gl.vertexAttrib3f(coords, 0.75, -0.5, 0.0);
-                gl.vertexAttribPointer(coords, 2, gl.FLOAT, false, 0, 0);
-                gl.enableVertexAttribArray(coords);
-                //gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-                var pointSize = gl.getAttribLocation(shaderProgram, "pointSize");
-                gl.vertexAttrib1f(pointSize, 3);
-
-                var color = gl.getUniformLocation(shaderProgram, "color");
-                gl.uniform4f(color, 0, 0, 1, 0.5);
-              }
-
-
-              function draw() {
-                var ratio = width / height;
-                
-                if( width > height){
-                  ratio = height / width;
-                }
-                for(var i = 0; i < vertexCount * 2; i += 2) {
-                  var dx = vertices[i] - mouseX,
-                    dy = vertices[i + 1] - mouseY,
-                      dist = Math.sqrt(dx * dx + dy * dy);
-                      if(dist < 0.2) {
-                        vertices[i] = mouseX + dx / dist * 0.2  ;
-                        vertices[i + 1] = mouseY + dy / dist *  0.2/ratio;
-                      }
-                      else {
-                        vertices[i] += Math.random() * 0.01 - 0.005;
-                        vertices[i + 1] += Math.random() * 0.01 - 0.005;
-                      }
-                }
-                gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(vertices));
-                gl.clear(gl.COLOR_BUFFER_BIT);
-                gl.drawArrays(gl.LINES, 0, vertexCount);
-
-                requestAnimationFrame(draw);
-              }
-
-              function map(value, minSrc, maxSrc, minDst, maxDst) {
-                return (value - minSrc) / (maxSrc - minSrc) * (maxDst - minDst) + minDst;
-              }
+          setTimeout(function(){ 
+            render();
+          }, 1000/30);
+        }
+        render();
+      });
 
 
 
-
-              function getShader(gl, id, type) {
-                var shaderScript, theSource, currentChild, shader;
-
-                shaderScript = document.getElementById(id);
-
-                if (!shaderScript) {
-                  return null;
-                }
-
-                theSource = shaderScript.text;
-
-                if (!type) {
-                  if (shaderScript.type == "x-shader/x-fragment") {
-                    type = gl.FRAGMENT_SHADER;
-                  } else if (shaderScript.type == "x-shader/x-vertex") {
-                    type = gl.VERTEX_SHADER;
-                  } else {
-                    // Unknown shader type
-                    return null;
-                  }
-                }
-                shader = gl.createShader(type);
-
-
-                gl.shaderSource(shader, theSource);
-
-                // Compile the shader program
-                gl.compileShader(shader);  
-
-                // See if it compiled successfully
-                if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {  
-                  alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));  
-                  gl.deleteShader(shader);
-                  return null;  
-                }
-
-                return shader;
-              }
-
-
-
-        })();
-}
+};
