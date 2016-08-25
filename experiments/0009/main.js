cf2eed7b06f2f58ff467bb0196b8ce503ef26182
jason <bjason@gmail.com>
2016-08-25 05:09:22 -0700

    shake mouse

--- experiments/0009/main.js
+++ experiments/0009/main.js
@@ -6,8 +6,16 @@ window.onload = function(){
       width = canvas.width = window.innerWidth,
       height = canvas.height = window.innerHeight,
       angle = 0,
-      inMotion = true,
-      shakes = 1000,
+      inMotion = false,
+      shakes = 3,
+      count= 0,
+      direction= 20,
+      xOff = 0,
+      max= 41,
+      min= -41,
+      moveCable = false,
+      cableDrawMax = 500,
+      cableDrawnCount = 0,
       speed = 0.1
 
 
@@ -34,25 +42,32 @@ window.onload = function(){
         // curve through the last two points
         ctx.quadraticCurveTo(points[i].x, points[i].y, points[i+1].x,points[i+1].y);
 
-        ctx.strokeStyle = '#E4AFFF';
+        ctx.strokeStyle = 'black';
         ctx.lineCap = "round";
-        ctx.lineWidth = 6;
+        ctx.lineWidth = 9;
         ctx.stroke();
     }
 
       var drawCord = function(){
         var points = [];
-        points.push({x: -img.width*0.02, y: 0});
-        points.push({x: -img.width*0.02, y: -180});
+        points.push({x: -img.width*0.02 + xOff, y: 0});
+        points.push({x: -img.width*0.02 + xOff, y: -180});
         
         for(var i = 4; i < 12; i++){
-          points.push({x: Math.sin(angle+i)*100 , y: -i*60  });
+          points.push({x: Math.sin(angle+i)*100 + xOff/i , y: -i*60  });
         }
 
-
-        if(inMotion){
-        angle+= speed;
+       
+        if(cableDrawnCount >= cableDrawMax){
+          moveCable = false;
+          cableDrawnCount = 0;
         }
+
+        if(moveCable || inMotion){
+          cableDrawnCount++;
+          angle+= speed;
+        }
+
         console.log(JSON.stringify(points));
         drawPoints(points);
 
@@ -68,7 +83,20 @@ window.onload = function(){
           ctx.translate( width / 2, height / 2);
 
           drawCord();
-          ctx.drawImage(img,-img.width/2, -img.height/2, img.width, img.height);
+          if(inMotion){
+            xOff+= direction;
+            if(xOff>= max || xOff <= min){
+              direction *=-1;
+              count++;
+            }
+            if(count>= shakes){
+              count = 0;
+              inMotion = false;
+              moveCable = true;
+              xOff = 0;
+            }
+          }
+          ctx.drawImage(img,-img.width/2 + xOff, -img.height/2, img.width, img.height);
 
 
           setTimeout(function(){ 
@@ -76,6 +104,11 @@ window.onload = function(){
           }, 1000/30);
         }
         render();
+      });
+
+      document.body.addEventListener('mousedown', function(){
+        inMotion = true;
+        xOff = 0;
       });
 
 
