24ce6d4cd179e9a01d34827f6e53f71491d27adf
jason <bjason@gmail.com>
2016-08-26 01:37:47 -0700

    scroll

--- experiments/0010/main.js
+++ experiments/0010/main.js
@@ -1,4 +1,24 @@
+$(window).load(function() {
+  console.log('jquery');
+  var scrolled = true;
+  $(document).on('click', function(){
+    if(scrolled){
+      $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+    } else {
+      $("html, body").animate({ scrollTop: 0 }, 1000);
+    }
+    scrolled = !scrolled;
+  });
+  });
+
 window.onload = function(){
   console.log('Loaded');
 
+
+    /*
+  document.addEventListener('mousedown', function(){
+    window.scrollTo(0,document.body.scrollHeight);
+
+  });
+  */
 };
